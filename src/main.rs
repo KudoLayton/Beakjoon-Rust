35b965abe638353ae950836d976490afa43c525f
H.Lee <lhslhg@gmail.com>
2021-06-08 17:56:48 +0900

    feat: BJ10998 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let nums: Vec<_> = input.split_whitespace().map(|x| x.parse::<i32>().expect("Failed")).collect();
+    println!("{}", nums[0] * nums[1]);
 }
