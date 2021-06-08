0b1bf35b05a06542e1f1616c2022d34b15bd0d57
H.Lee <lhslhg@gmail.com>
2021-06-08 10:33:16 +0900

    feat: BJ2577 final

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use std::convert::TryFrom;
+
 fn main() {
-    println!("Hello, world!");
+    let mut mul_val:i32 = 1;
+    for _ in 0..3{
+        let mut input_str = String::new();
+        io::stdin().read_line(&mut input_str).expect("Failed");
+        mul_val = mul_val * input_str.trim().parse::<i32>().expect("Failed");
+    }
+
+    let mut digit_list : Vec<i32> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    while mul_val != 0 {
+        digit_list[usize::try_from(mul_val % 10).expect("Failed")] += 1;
+        mul_val = (mul_val - (mul_val % 10)) / 10;
+    }
+
+    for item in digit_list.iter() {
+        println!("{}", item);
+    }
 }
