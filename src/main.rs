8adbbe4d5b51741928aa624e3c54281539bbec29
H.Lee <lhslhg@gmail.com>
2021-06-08 13:02:12 +0900

    feat: BJ1152 submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  let mut a = String::new();
+  io::stdin().read_line(&mut a).expect("Failed");
+  let v = a.split_whitespace();
+  print!("{}", v.count());
 }
