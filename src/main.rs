d6b287501ea5338010709b68afa54fe467e6e720
H.Lee <lhslhg@gmail.com>
2021-06-08 14:41:24 +0900

    feat: BJ2884 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let time: Vec<_> = input.split_whitespace().map(|x| x.parse::<i32>().expect("Failed")).collect();
+    match (time[0] > 0, time[1] >= 45){
+        (_, true) => println!("{} {}", time[0], time[1] - 45),
+        (true, false) => println!("{} {}", time[0] - 1, time[1] + 15),
+        (false, false) => println!("23 {}", time[1] + 15),
+    }
 }
