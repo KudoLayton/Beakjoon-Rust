a9cf8b0c5a7a4048bd9eb871b72645ef903e0337
H.Lee <lhslhg@gmail.com>
2021-06-08 10:57:36 +0900

    feat: Time out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let rep = input.trim().parse::<i32>().expect("Failed");
+
+    for i in 0..rep{
+        println!("{}", i+1);
+    }
 }
