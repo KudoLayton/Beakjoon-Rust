ebca60b06d57b3eae31d06049cc5650bcc256bb4
H.Lee <lhslhg@gmail.com>
2021-06-08 18:04:39 +0900

    feat: BJ11654 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let input_chars: Vec<_> = input.trim().chars().collect();
+    println!("{}", input_chars[0] as u8);
 }
