80ef791580dbbc6cd872806fc7aa7d1e4900c8e3
H.Lee <lhslhg@gmail.com>
2021-06-08 15:36:22 +0900

    feat: BJ9498

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let input_num = input.trim().parse::<i32>().expect("Failed") / 10;
+    match input_num{
+        10 => println!("A"),
+        9 => println!("A"),
+        8 => println!("B"),
+        7 => println!("C"),
+        6 => println!("D"),
+        _ => println!("F"),
+    }
 }
