c7ab8e2d17b98602680974d945f7e086cfa20e3a
H.Lee <lhslhg@gmail.com>
2021-06-08 11:22:24 +0900

    feat: BJ10809 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let input_chars = input.chars();
+    for c in 'a'..='z'{
+        let mut loop_input_chars = input_chars.clone();
+        match loop_input_chars.position(|x| x == c){
+            Some(x) => print!("{} ", x),
+            None => print!("-1 "),
+        }
+    }
 }
