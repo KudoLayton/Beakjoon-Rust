749e794810666478d1e66bda447f383c0fdf361b
H.Lee <lhslhg@gmail.com>
2021-06-08 11:05:09 +0900

    feat: BJ2741 submit

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     io::stdin().read_line(&mut input).expect("Failed");
     let rep = input.trim().parse::<i32>().expect("Failed");
 
+    let mut output = String::new();
     for i in 0..rep{
-        println!("{}", i+1);
+        output.push_str(format!("{}\n", i+1).as_str());
     }
+    print!("{}", output);
 }
