3d060ee6773986092fb2f91b4855f61a17739dd0
H.Lee <lhslhg@gmail.com>
2021-06-08 15:15:36 +0900

    feat: BJ3052 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut number_log: HashMap<i32, bool> = HashMap::new();
+    for _ in 0..10{
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("False");
+        number_log.insert(input.trim().parse::<i32>().expect("Failed") % 42, true);
+    }
+    println!("{}", number_log.len());
 }
