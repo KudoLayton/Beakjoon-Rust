0885117c3721b8ea17dfac9cb5280775b1a21d10
H.Lee <lhslhg@gmail.com>
2021-06-08 10:48:21 +0900

    feat: BJ2675 submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let test_case_num = input.trim().parse::<i32>().expect("Failed");
+
+    for _ in 0..test_case_num{
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed");
+        let split_input_list:Vec<_> = input.split_whitespace().collect();
+        let rep_num = split_input_list[0].parse::<i32>().expect("Failed");
+        let char_list = split_input_list[1].chars();
+        for item in char_list{
+            for _ in 0..rep_num{
+                print!("{}", item);
+            }
+        }
+        print!("\n");
+    }
 }
