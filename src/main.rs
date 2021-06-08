984c8a070c82d7f7ff2ab87d6601f6fc34c7cc3b
H.Lee <lhslhg@gmail.com>
2021-06-08 14:28:32 +0900

    feat: BJ2562 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  let mut max_val = 0;
+  let mut max_val_idx = -1;
+  for i in 0..9{
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let input_val = input.trim().parse::<i32>().expect("Failed");
+    if max_val < input_val{
+      max_val = input_val.clone();
+      max_val_idx = i.clone();
+    }
+  }
+  println!("{}", max_val);
+  println!("{}", max_val_idx + 1);
 }
