6d2c35402576e1031098805f718422b39280b565
H.Lee <lhslhg@gmail.com>
2021-06-08 14:23:09 +0900

    feat: BJ1330 submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let diff = input.split_whitespace().map(|x| x.parse::<i32>().expect("Failed")).fold(0, |acc, x| -(acc + x));
+    match diff {
+        0 => println!("=="),
+        _ => match diff / diff.abs() {
+            1 => println!(">"),
+            _ => println!("<"),
+        }
+    }
 }
