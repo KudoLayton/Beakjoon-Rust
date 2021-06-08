d83648929a68573ee893d6852436c98e3778e273
H.Lee <lhslhg@gmail.com>
2021-06-08 12:58:30 +0900

    feat: BJ1000 submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).expect("Failed");
+    let v = a.split_whitespace();
+    let r = v.clone().count();
+    match r {
+        2 => print!("{}", v.fold(0, |acc:i32, x| acc + x.parse::<i32>().expect("Nope"))),
+        _ => (),
+    }
 }
