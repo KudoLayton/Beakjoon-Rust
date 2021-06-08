0f8396291bd29f583807c5f775b1a47ed7e514bb
H.Lee <lhslhg@gmail.com>
2021-06-08 12:55:30 +0900

    feat: BJ2753 submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let val = input.trim().parse::<i32>().expect("Failed");
+    match val % 400 {
+        0 => println!("1"),
+        _ => {
+            match val % 100 {
+                0 => println!("0"),
+                _ => {
+                    match val % 4 {
+                        0 => println!("1"),
+                        _ => println!("0"),
+                    }
+                },
+            }
+        },
+    }
 }
