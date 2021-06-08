3319019c7492c7a018c7d9f7e29dd91b8f0053cb
H.Lee <lhslhg@gmail.com>
2021-06-08 11:44:28 +0900

    feat: BJ2742 submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let rep = input.trim().parse::<i32>().expect("Failed");
+
+    let mut output = String::new();
+    for i in 0..rep{
+        output.push_str(format!("{}\n", rep - i).as_str());
+    }
+    print!("{}", output);
 }
