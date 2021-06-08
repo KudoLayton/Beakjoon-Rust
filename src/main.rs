b0de8d6e97cd2528ca0247f48a79524d3a14449f
H.Lee <lhslhg@gmail.com>
2021-06-08 13:00:53 +0900

    feat: BJ1008 submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  let mut a = String::new();
+  io::stdin().read_line(&mut a).expect("Failed");
+  let v = a.split_whitespace();
+  let r = v.clone().count();
+  match r {
+    2 => {
+      let m:Vec<f64>  = v.map(|x| x.parse::<f64>().expect("Not working")).collect();
+      print!("{:.9}", m[0] / m[1]);
+    }
+    _ => (),
+  }
 }
