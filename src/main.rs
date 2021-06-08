a280d95e2ef6ce8ed85bbbfc81dfdc2753d3fd90
H.Lee <lhslhg@gmail.com>
2021-06-08 15:04:00 +0900

    feat: BJ2920 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    match input.trim() {
+        "1 2 3 4 5 6 7 8" => println!("ascending"),
+        "8 7 6 5 4 3 2 1" => println!("descending"),
+        _ => println!("mixed")
+    }
 }
