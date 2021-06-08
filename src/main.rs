42b0dece9dedae65377f207a6c88234749e7c4b0
H.Lee <lhslhg@gmail.com>
2021-06-08 14:26:57 +0900

    feat: BJ2475 submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  let mut input = String::new();
+  io::stdin().read_line(&mut input).expect("Failed");
+  let sqr_sum = input.split_whitespace().fold(0, |acc, x| acc + (x.parse::<i32>().expect("Failed").pow(2)));
+  println!("{}", sqr_sum % 10);
 }
