34b65ac3d82a56b3a64ad3ffa287067114611d0c
H.Lee <lhslhg@gmail.com>
2021-06-08 14:55:37 +0900

    feat: BJ2908 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let rev_num = input.trim().chars().rev().collect::<String>();
+    let rev_num = rev_num.split_whitespace().map(|x| x.parse::<i32>().expect("Failed"));
+    let max_num = rev_num.fold(0, |acc, x| x.max(acc));
+    println!("{}", max_num);
 }
