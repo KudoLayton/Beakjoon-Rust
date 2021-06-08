3be13812439b67769bf0791180033fe53d3cf616
H.Lee <lhslhg@gmail.com>
2021-06-09 08:50:55 +0900

    feat: BJ11720 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let number_list= input.trim().chars().map(|x| x.to_string().parse::<i32>().expect("Failed"));
+    println!("{}", number_list.fold(0, |acc, x| acc + x));
 }
