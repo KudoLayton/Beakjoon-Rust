e32ce0b6e04d74fc6eaa501815f3aeb908fa65f9
H.Lee <lhslhg@gmail.com>
2021-06-08 17:40:18 +0900

    feat: BJ10951 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input : Vec<u8> = Vec::new();
+    io::stdin().read_to_end(&mut input).expect("Failed");
+    let input_str:String = input.iter().map(|x| char::from(x.clone())).collect();
+    let input_lines = input_str.as_str().trim().split("\n");
+    for line in input_lines{
+        let nums: Vec<_> = line.clone().split_whitespace().map(|x| x.parse::<i32>().expect("Failed")).collect();
+        println!("{}", nums[0] + nums[1]);
+    }
 }
