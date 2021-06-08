57a25597186a56e7d264e8d50ece01829fa45d7f
H.Lee <lhslhg@gmail.com>
2021-06-08 16:24:27 +0900

    feat: BJ10818 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+
+    let input_list = input.trim().split_whitespace().map(|x| x.parse::<i32>().expect("Failed"));
+    let max_iter = input_list.clone();
+    let min_iter = input_list.clone();
+    println!("{} {}", min_iter.min().unwrap(), max_iter.max().unwrap());
 }
