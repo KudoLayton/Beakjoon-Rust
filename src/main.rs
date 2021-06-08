32534408e40b037407e86068f84b0f48d0ac0417
H.Lee <lhslhg@gmail.com>
2021-06-08 15:29:21 +0900

    feat: BJ5958 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let case_num = input.trim().parse::<i32>().expect("Failed"); 
+    for _ in 0..case_num{
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed");
+        println!("{}", input.trim().split("X").fold(0, |acc, x| {
+            let length = x.chars().count();
+            acc + (length * (length + 1) / 2)
+        }))
+    }
 }
