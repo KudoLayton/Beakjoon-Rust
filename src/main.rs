695092f446e12b1f32c181ef6b66cbe2996b87d5
H.Lee <lhslhg@gmail.com>
2021-06-09 16:50:42 +0900

    feat: BJ1085 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let input_params: Vec<_> = input.split_whitespace().map(|x| x.parse::<i32>().expect("Failed")).collect();
+    let hor_min = input_params[0].min(input_params[2] - input_params[0]);
+    let ver_min = input_params[1].min(input_params[3] - input_params[1]);
+    println!("{}", hor_min.min(ver_min));
 }
