f909e584a28abd30c26583797ae1799b7c4d7883
H.Lee <lhslhg@gmail.com>
2021-06-09 17:29:43 +0900

    feat: BJ1181 pass

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use std::io;
+use std::collections::HashMap;
+use std::cmp::Ordering;
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed");
-    let input_params: Vec<_> = input.split_whitespace().map(|x| x.parse::<i32>().expect("Failed")).collect();
-    let hor_min = input_params[0].min(input_params[2] - input_params[0]);
-    let ver_min = input_params[1].min(input_params[3] - input_params[1]);
-    println!("{}", hor_min.min(ver_min));
+    let iter_num = input.trim().parse::<usize>().expect("Failed");
+    let mut dict:HashMap<String, bool> = HashMap::new();
+    for _ in 0..iter_num{
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed");
+        dict.insert(input, true);
+    }
+    let dict:Vec<_> = dict.keys().collect();
+    let mut dict:Vec<_> = dict.iter().map(|x| x.trim()).collect();
+    dict.sort_by(|x, y| 
+        if x.len() > y.len(){
+            return Ordering::Greater;
+        } else if x.len() < y.len(){
+            return Ordering::Less;
+        }else{
+            return x.cmp(y);
+        }
+    );
+    for dict_string in dict{
+        println!("{}", dict_string)
+    }
 }
