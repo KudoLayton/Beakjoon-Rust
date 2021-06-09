97e7b3c16f9f17046c2d0562b687a7eb63eb520d
H.Lee <lhslhg@gmail.com>
2021-06-09 17:43:34 +0900

    feat: BJ1259 pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop{
+        let mut input = String::new();
+        io::stdin().read_line(&mut input);
+        match input.trim(){
+            "0" => break,
+            _ => {
+                let x:i32 = input.trim().parse().expect("Failed");
+                let rev_x:i32 = input.trim().chars().rev().collect::<String>().parse::<i32>().expect("Failed");
+                if x == rev_x {
+                    println!("yes");
+                } else {
+                    println!("no");
+                }
+            }
+        }
+    }
 }
